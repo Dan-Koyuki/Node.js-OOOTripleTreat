c743c99b6239936eaee129416585d67de7790d70
Dan-Koyuki <dankoyuki398@gmail.com>
2023-12-04 21:32:26 +0700

    add register and login endpoint using moongose, jwt, and joi

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const register = require('./routes/register');
+const login = require('./routes/login');
+
 
 const products = require('./products')
 
 const app = express();
 
+require('dotenv').config();
+
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/register", register);
+app.use("/api/login", login);
 
 app.get('/', (req, res) => {
   res.send("Welcome to my API...");
@@ -17,6 +26,11 @@ app.get('/products', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const URI = process.env.DB_URI;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose.connect(URI).then(() => console.log("MongoDB Connected!"))
+.catch((err) => console.log("Failed", err.message));
--- products.js
+++ products.js
@@ -2,28 +2,28 @@ const products = [
   {
     id: 1,
     name: 'Deca Driver',
-    desc: 'Driver to Transform to Decade',
+    desc: 'Driver of Kamen Rider Decade',
     price: 330,
     image: "https://res.cloudinary.com/dankoyuki/image/upload/v1701574361/OOO-Triple_Treat/wvukbs5rt70ybmvudrds.webp"
   },
   {
     id: 2,
     name: 'Build Driver',
-    desc: 'Driver to Transform to Build',
+    desc: 'Driver of Kamen Rider Build',
     price: 129,
     image: "https://res.cloudinary.com/dankoyuki/image/upload/v1701574801/OOO-Triple_Treat/vaireh7bl4c8rgm22k3g.jpg"
   },
   {
     id: 3,
     name: 'Desire Driver',
-    desc: 'Driver to Transform to any Geats Rider',
+    desc: 'Driver of Geats Rider',
     price: 106.20,
     image: "https://res.cloudinary.com/dankoyuki/image/upload/v1701574362/OOO-Triple_Treat/q0ig7fafqfc6qkpoykcv.webp"
   },
   {
     id: 4,
     name: 'Set ID Core',
-    desc: 'ID Core to place in Desire Driver',
+    desc: 'Desire Grand Prix ID Core',
     price: 70,
     image: "https://res.cloudinary.com/dankoyuki/image/upload/v1701574361/OOO-Triple_Treat/btmqjhh612fcpjferttl.jpg"
   }
